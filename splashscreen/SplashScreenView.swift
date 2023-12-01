528f73b8eda89aa5fd615abca228a8d0be14884c
jindongdu <djd28176@hotmail.com>
2023-12-01 15:21:54 -0500

    initial

---
+++ splashscreen/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  splashscreen
+//
+//  Created by Jindong Du on 12/1/23.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
