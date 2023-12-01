ce2024aa7257933b7502f72979f74da8529f9293
jindongdu <djd28176@hotmail.com>
2023-12-01 15:05:45 -0500

    Initial Commit

---
+++ splashscreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  splashscreen
+//
+//  Created by Jindong Du on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ splashscreen/splashscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  splashscreenApp.swift
+//  splashscreen
+//
+//  Created by Jindong Du on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct splashscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
