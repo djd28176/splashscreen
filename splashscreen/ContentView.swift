9991a0ee6f0a6c88d76d054c8565a7338be3c52e
jindongdu <djd28176@hotmail.com>
2023-12-01 15:22:15 -0500

    splash screen

--- splashscreen/ContentView.swift
+++ splashscreen/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            Text("Home Screen of LCO app")
+                .foregroundColor(.white)
+                .font(.system(size: 30))
+                .bold()
+                .padding()
         }
-        .padding()
     }
 }
 
--- splashscreen/SplashScreenView.swift
+++ splashscreen/SplashScreenView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @State var isActive: Bool = false
+    @State private var size = 0.7
+    @State private var opacity = 0.4
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive{
+            ContentView()
+        }else{
+            VStack{
+                Image("mascot")
+                Text("LearnCodeOnline")
+                    .font(.system(size: 40))
+                    .foregroundStyle(.black)
+                    .opacity(0.7)
+            }
+            .scaleEffect(size)
+            .opacity(opacity)
+            .onAppear{
+                withAnimation(.easeIn(duration: 1.0)){
+                    self.size = 1.1
+                    self.opacity = 1.0
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now()+1.5){
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+            
+        }
     }
 }
 
